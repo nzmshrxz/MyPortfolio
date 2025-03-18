3403f40f4422c31a2e78fafe53710cb232d47b69
Nazam Shiraz <141271901+nzmshrxz@users.noreply.github.com>
2025-03-19 00:47:30 +0530

    Update Hero.jsx

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/CV.pdf';
+import cv from '../../assets/cv.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
@@ -56,7 +56,7 @@ function Hero() {
           With a passion for developing modern React web apps for commercial
           businesses.
         </p>
-        <a href={CV} download>
+        <a href={cv} download>
           <button className="hover">Resume</button>
         </a>
       </div>
